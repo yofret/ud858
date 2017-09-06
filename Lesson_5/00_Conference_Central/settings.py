66f319e13673bc4d4ad7e9af5f784b4b4b1c66ec
Yofret Rios <yofret.rios@jobsity.com>
2017-09-06 08:41:38 -0500

    Passing information to lesson 5

--- Lesson_5/00_Conference_Central/settings.py
+++ Lesson_5/00_Conference_Central/settings.py
@@ -10,9 +10,12 @@ created/forked from conference.py by wesc on 2014 may 24
 
 """
 
+# dev_appserver.py app.yaml
+# gcloud app deploy app.yaml --project pythoncourse-168613 --version 1
+# 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '950159804578-fv97sh9i29l0jpi889j7td8n1k0n036g.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
